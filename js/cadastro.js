5f3803dcdf988e45080db479fe71ed57b7d0a808
Nath <nathterribile@hotmail.comh>
2024-08-21 00:54:56 -0300

    api cadastro

--- js/cadastro.js
+++ js/cadastro.js
@@ -2,17 +2,23 @@ async function cadastrar() {
     const login = document.getElementById("userToBe").value;
     const senha = document.getElementById("passwordToBe").value;
 
-
-
-    fetch("http://localhost:8080/usuario/cadastro,{
-        method:"POST",
-        headers: {"Content-type": "application/json; charset=UTF-8"},
-        body: JSON.stringify({
-                login: login,
-                senha: senha
-        }),
+    fetch("http://localhost:8080/usuario/cadastrar/login?usuario=" + login + "&senha=" + senha, {
+        method: "POST"
     })
-        .then((response) => response.json())
-        .then((json) => console.log(json));
-}
-
+        .then(
+            (response) => {
+                if (response.ok) {
+                    window.location.replace("./pages/admin.html");
+                }
+                return Promise.reject(response)
+            }
+        ).catch((response) => {
+            if (response.status == 401) {
+                document.getElementById("login_error").style.visibility = "visible";
+            } else {
+                document.getElementById("login_error").style.visibility = "visible";
+                document.getElementById("login_error").value = response;
+            }
+    
+        });
+    }

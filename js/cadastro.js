e9132bc0bb31c6f7f23225667a0ef1d806ea9daf
m315930 <nathalia.t.nascimento@bradescoseguros.com.br>
2025-01-01 11:58:26 -0300

    atualização POST cadastro

--- js/cadastro.js
+++ js/cadastro.js
@@ -1,22 +1,31 @@
 async function cadastrar() {
     const login = document.getElementById("userToBe").value;
     const senha = document.getElementById("passwordToBe").value;
+    const data = {usuario: login, senha:senha};
 
 
-    fetch("http://localhost:8080/usuario/cadastrar/login?usuario=" + login + "&senha=" + senha, {
-        method: "POST"
-    }).then(
-                 (response) => {
-                    return Promise.reject(response)
-                 }
-             ).catch((response) => {
-                 if (response.status == 401) {
-                     document.getElementById("login_error").style.visibility = "visible";
-                 } else {
-                     document.getElementById("login_error").style.visibility = "visible";
-                     document.getElementById("login_error").value = response;
-                 }
-         
-                 })
-}
-     
+    fetch("http://localhost:8080/usuario/cadastrar", {
+        method: "POST",
+        headers:{
+                "Accept": "application/json",
+                "Content-Type":"application/json"
+        },
+        body:JSON.stringify(data)
+            })
+                .then(
+                    (response) => {
+                        if (response.ok) {
+                            window.location.replace("./pages/admin.html");
+                        }
+                        return Promise.reject(response)
+                    }
+                ).catch((response) => {
+                    if (response.status == 401) {
+                        document.getElementById("login_error").style.visibility = "visible";
+                    } else {
+                        document.getElementById("login_error").style.visibility = "visible";
+                        document.getElementById("login_error").value = response;
+                    }
+        
+            });
+        }

f899cae3f1fa6d06a8a9b8858204a3328ea1cee1
nathterribile <82793244+nathterribile@users.noreply.github.com>
2025-06-16 21:10:26 -0300

    Update agendamento.js
    certificado SSL

--- js/agendamento.js
+++ js/agendamento.js
@@ -25,7 +25,7 @@ async function agendar() {
         
     };
 
-    const response = await fetch("https://172.29.208.1:8080/os", {
+    const response = await fetch("https://172.29.208.1:8443/os", {
         method: "POST",
         headers:{
                 "Accept": "application/json",
